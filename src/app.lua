eea6b4991ce728449a9d12905509c570c001af95
Dmitry Kasimovskiy <dmitry.kasimovskiy@visma.com>
2022-02-21 13:35:12 +0300

    Add rate limit
    Add rate limit check for all HTTP requests
    Add custom log format for all HTTP requests
    Disabled tarantool console
    Imrovements and cosmetics

--- src/app.lua
+++ src/app.lua
@@ -20,43 +20,76 @@ box.cfg {
 	read_only   = false
 }
 
-local function bootstrap()
-	local space = box.schema.space.create('kv', {if_not_exists = true})
-	space:format({
+box.once("bootstrap", function()
+	local kv_storage = box.schema.space.create('kv', {if_not_exists = true})
+	kv_storage:format({
 		{name = 'k', type = 'string', is_nullable=false},
-		{name = 'v', type = 'string', is_nullable=false}
+		{name = 'v', type = 'any', is_nullable=false}
 	})
-	space:create_index('primary', {
-		unique = true,
-		if_not_exists = true,
+	kv_storage:create_index('primary', {unique = true, if_not_exists = true,
 		parts = {{'k', 'string'}}
 	})
 
-	box.schema.user.create('kv', { password = 'secret' })
-	box.schema.user.grant('kv', 'read,write,execute', 'space', 'kv')
+	box.schema.user.create('kv_user', { password = 'secret', if_not_exists = true })
+	box.schema.user.grant('kv_user', 'read,write,execute', 'space', 'kv')
+end)
 
-	box.schema.user.create('repl', { password = 'replication' })
-	box.schema.user.grant('repl', 'replication')
-end
+local log = require('log')
+local pickle = require('pickle')
 
--- for first run create a space and add set up grants
-box.once('replica', bootstrap)
-
--- enabling console access
-console = require('console')
-console.listen('127.0.0.1:3302')
-
+-- Configure HTTP server
 local http_server = require('http.server')
 local httpd = http_server.new('0.0.0.0', 8080, {
-	log_requests = true,
+	log_requests = false,
 	log_errors = true,
 	header_timeout = 1
 })
 
 
+-- Confirure throttle service
+local APP_RL_TOTAL = tonumber(os.getenv('APP_RATE_LIMIT_TOTAL'))
+local APP_RL_TIME_FRAME_SEC = tonumber(os.getenv('APP_RATE_LIMIT_TIME_FRAME_SEC'))
+
+if APP_RL_TOTAL ~= nil and APP_RL_TIME_FRAME_SEC ~= nil and APP_RL_TOTAL > 0 and APP_RL_TIME_FRAME_SEC > 0 then
+	local clock = require('clock')
+
+	RATE_LIMIT_HEADER = 'x-rate-limit'
+
+	local prev_rate = APP_RL_TOTAL
+	local curr_rate = 0
+	local start_time = clock.time()
+
+	local function sliding_window_rate_limit(r)
+		local now = clock.time()
+		if (now - start_time) > APP_RL_TIME_FRAME_SEC then
+			start_time = now
+			prev_rate = curr_rate
+			curr_rate = 0
+		end
+
+		local slide_prct = (APP_RL_TIME_FRAME_SEC - (now - start_time)) / APP_RL_TIME_FRAME_SEC
+		local estimated_count = (prev_rate * slide_prct) + curr_rate
+		log.debug('slide: %s prev: %s current: %s estimated: %s', slide_prct, prev_rate, curr_rate, estimated_count)
+
+		if estimated_count > APP_RL_TOTAL then
+			r.headers[RATE_LIMIT_HEADER] = 1
+			return
+		end
+
+		curr_rate = curr_rate + 1
+	end
+
+	httpd:hook('before_dispatch', function (h, req) sliding_window_rate_limit(req) end)
+end
+
+
+httpd:hook('after_dispatch', function (cx, resp)
+	local s = pickle.pack('A', resp['body'] or '')
+	log.info('"%s %s HTTP/%d.%d" %s %i', cx.method, cx.path, cx.proto[1], cx.proto[2], resp['status'], #s)
+end)
+
 httpd:route({method = 'GET', path = '/kv/:id'}, "kv#get")
 httpd:route({method = 'POST', path = '/kv'}, "kv#create")
 httpd:route({method = 'PUT', path = '/kv/:id'}, "kv#update")
 httpd:route({method = 'DELETE', path = '/kv/:id'}, "kv#delete")
-
 httpd:start()
--- src/controllers/kv.lua
+++ src/controllers/kv.lua
@@ -1,91 +1,99 @@
 local json = require('json')
-local codes = require('http.codes')
 local log = require('log')
+local codes = require('http.codes')
 
-function http_created() return http_response(201) end
-function http_no_content() return http_response(204) end
-function http_bad_request() return http_response(400) end
-function http_not_found() return http_response(404) end
-function http_conflict() return http_response(409) end
+function http_created(req) return http_response(req, 201) end
+function http_no_content(req) return http_response(req, 204) end
+function http_bad_request(req) return http_response(req, 400) end
+function http_not_found(req) return http_response(req, 404) end
+function http_conflict(req) return http_response(req, 409) end
+function http_too_many_requests(req) return http_response(req, 429) end
+function http_server_error(req) return http_response(req, 500) end
 
-function http_response(http_status_code)
-	if http_status_code == nil or codes[http_status_code] == nil then 
+function http_response(req, http_status_code)
+	if http_status_code == nil or codes[http_status_code] == nil then
 		http_status_code = 500
 	end
 
-	return {status = http_status_code, body = http_status_code .. ' ' .. codes[http_status_code] .. "\n" }
+	local resp = req:render({text = http_status_code .. ' ' .. codes[http_status_code] .. "\n"})
+	resp.status = http_status_code
+	return resp
 end
 
-local function get_item(r)
-	local id = r:stash('id')
-	if id == nil then
-		return http_not_found()
-	end
-
-	local tuple = box.space.kv:get{id};
-	if tuple == nil then
-		return http_not_found()
-	end
-
-	return {status = 200, body = tuple['v']}
+local function get_item(req, id, tuple)
+	local resp = req:render({json = tuple['v']})
+	resp.status = 200
+	return resp
 end
 
-local function create_item(r)
-	local data = r:json()
+local function create_item(req)
+	local data = req:json()
 
 	if data == nil or data['key'] == nil or data['value'] == nil then
-		return http_bad_request()
-	end
-	log.info('ITEM key:%s value:%s', data['key'], data['value'])
-
-	local status, error = pcall(function() box.space.kv:insert{data['key'], json.encode(data['value'])} end)
-	if status == false then
-		log.info('INSERT DUP: %s %s', status, error)
-		return http_conflict()
+		return http_bad_request(req)
 	end
 
-	return http_created()
+	local status, error = pcall(function() box.space.kv:insert{tostring(data['key']), data['value']} end)
+	if not status then
+		log.warn('INSERT key %s failed: %s', data['key'], error)
+		return http_conflict(req)
+	end
+
+	return http_created(req)
 end
 
-
-local function update_item(r)
-	local id = r:stash('id')
-	if id == nil then
-		return http_not_found()
-	end
-
-	local data = r:json()
+local function update_item(req, id, tuple)
+	local data = req:json()
 	if data == nil or data['value'] == nil then
-		return http_bad_request()
+		return http_bad_request(req)
 	end
 
-	local tuple = box.space.kv:get{id};
-	if tuple == nil then
-		return http_not_found()
+	local status, error = pcall(function() box.space.kv:replace{id, data['value']} end)
+	if not status then
+		log.warn('UPDATE key %s failed: %s', id, error)
+		return http_server_error(req)
 	end
-	
-	box.space.kv:replace{id, json.encode(data['value'])};
-	return http_no_content()
+
+	return http_no_content(req)
 end
 
-local function delete_item(r)
-	local id = r:stash('id')
-	if id == nil then
-		return http_not_found()
+local function delete_item(req, id, tuple)
+	local status, error = pcall(function() box.space.kv:delete{id} end)
+	if not status then
+		log.warn('DELETE key %s failed: %s', id, error)
+		return http_server_error(req)
 	end
 
-	local tuple = box.space.kv:get{id};
-	if tuple == nil then
-		return http_not_found()
+	return http_no_content(req)
+end
+
+local function request_handler(req)
+	if req.headers[RATE_LIMIT_HEADER] ~= nil then
+		return http_too_many_requests(req)
 	end
-	
-	box.space.kv:delete{id};
-	return http_no_content()
+
+	if req.method ~= 'POST' then
+		local id = req:stash('id')
+		local tuple = box.space.kv:get{id}
+		if tuple == nil then
+			return http_not_found(req)
+		end
+
+		if req.method == 'GET' then
+			return get_item(req, id, tuple)
+		elseif req.method == 'PUT' then
+			return update_item(req, id, tuple)
+		elseif req.method == 'DELETE' then
+			return delete_item(req, id, tuple)
+		end
+	else
+		return create_item(req)
+	end
 end
 
 return {
-	get = function(self) return get_item(self) end,
-	create = function(self) return create_item(self) end,
-	update = function(self) return update_item(self) end,
-	delete = function(self) return delete_item(self) end
+	get = function (self) return request_handler(self) end,
+	create = function (self) return request_handler(self) end,
+	update = function (self) return request_handler(self) end,
+	delete = function (self) return request_handler(self) end
 }
